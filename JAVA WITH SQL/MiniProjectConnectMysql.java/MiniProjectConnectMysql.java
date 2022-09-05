2b56f61bebd9c6e8886e7380a879929f03053d1a
JJin0728 <gydud99@gmil.com>
2022-09-05 09:49:13 +0900

    test

--- JAVA WITH SQL/MiniProjectConnectMysql.java/MiniProjectConnectMysql.java
+++ JAVA WITH SQL/MiniProjectConnectMysql.java/MiniProjectConnectMysql.java
@@ -6,7 +6,8 @@ Pubilc class MiniProjectConnectMysql {
        //    Paramaters내용 확인
         //3.패스워드 입력후 승인확인
         //4.연결 폴더와 클래스 생성
-        //5.코드 명령어 입력후 출력 확인
+        //5.새로운 쿼리에서 SELECT명령어 입력후 
+            출력확인
     
         return ;
     }
