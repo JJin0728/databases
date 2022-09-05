d414d3a82163cb391b6cfc09dc65c8ca5554dde4
JJin0728 <gydud99@gmil.com>
2022-09-05 12:06:19 +0900

    miniprojest

--- JAVA WITH SQL/Miniproject01/src/MiniProjectConnectMysql.java
+++ JAVA WITH SQL/Miniproject01/src/MiniProjectConnectMysql.java
@@ -4,31 +4,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MiniProjectConnectMysql {
+
 
     public class MiniProjectConnectMysql {
-    public static void main(string[]args){
+    public static void main(String[]args){
         // VS code에서 폴더 만들기, class파일 만들기
         // MySQL  열기 -->my sql-connector J
         // connect to database 클릭
-        final String USER = "root ";}
-        final  String PASS = "TB2022B ";
-         final String DB_URL = "jdbc:mysql://localhost/world";
-         String Query = "select ID,Name,Population "+
-                        "from city "+
-                        "where 1 = 1 "+
-                        "and Population >= 9230000 ";
-    
-        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+final String USER = "root ";
+final String PASS = "TB2022B ";
+final String DB_URL = "jdbc:mysql://localhost/world";
+String QUERY = "select Code, Name, Population "+
+"from country "+
+"where 1=1 " +
+"and Name Like '%an%' "
+;
+        
         try{
             Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(Query);
+            ResultSet rs = stmt.executeQuery(QUERY);
             while (rs.next()) {
                 // Retrieve by column name
-                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print("Code: " + rs.getInt("code"));
                 System.out.print(", NAME: " + rs.getString("name"));
-
+            }
                 System.out.println(", Population: " + rs.getInt("population"));
         }catch(SQLException e) {
             e.printStackTrace();
@@ -39,4 +39,3 @@ public class MiniProjectConnectMysql {
     
     }
 }
-
